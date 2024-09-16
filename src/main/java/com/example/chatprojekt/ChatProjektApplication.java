2326df433035423238d480a4d906ac77142cd98d
Slimecaster <nd070503@gmail.com>
2024-09-16 13:17:06 +0200

    hi

---
+++ src/main/java/com/example/chatprojekt/ChatProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatprojekt/ChatProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
