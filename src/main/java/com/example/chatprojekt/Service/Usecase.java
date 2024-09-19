f79214e8f22f8fabc558cf17066b9b158c3b1ae4
amalieasborg <amalieasborg@gmail.com>
2024-09-19 10:12:33 +0200

    Puttede nogle db-metoder i usecase

--- src/main/java/com/example/chatprojekt/Service/Usecase.java
+++ src/main/java/com/example/chatprojekt/Service/Usecase.java
@@ -1,4 +1,28 @@
 package com.example.chatprojekt.Service;
 
+import com.example.chatprojekt.Model.Client;
+import com.example.chatprojekt.Repository.DBController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class Usecase {
+    @Autowired
+    private DBController dbController=new DBController(new JdbcTemplate());
+    private Client client=new Client();
+
+
+    public Client createUpdateClient(Client client){
+        return dbController.createUpdateUser(client);
+    }
+
+    public Optional<Client> findClientByClientname(String clientname){
+        return dbController.findClientByClientname(clientname);
+    }
+
+
+
 }
